7fe36ce797a9a383f3dcb0fcc8c1ca948703dfb9
Renatocirilo <renato_cirilo@hotmail.com>
2020-01-23 23:17:15 -0300

    Mobile

--- js/pay-me.js
+++ js/pay-me.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
         document.getElementById("myBtn").style.display = "block";
     } else {
         document.getElementById("myBtn").style.display = "none";
